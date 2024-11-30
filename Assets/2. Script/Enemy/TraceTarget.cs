aa4d25948b8f7d7997dee6710368d5019f036010
ee <eee@ww.com>
2024-11-30 10:28:06 +0900

    [개선] 적 추적 개선
    - 여러 적이 끝까지 추적하면 플레이어를 밀어서 불편함 -> 추적 중지 거리를 설정하였음

--- Assets/2. Script/Enemy/TraceTarget.cs
+++ Assets/2. Script/Enemy/TraceTarget.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 public class TraceTarget : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [Header("���� ���")]
     [SerializeField] private GameObject target;
+    [Header("���� ���� ����")]
     [SerializeField] private float distance = 5f;
+    [Header("���� ���� �Ÿ�")]
+    [SerializeField] private float stopDistance = 1f;
+    [Header("�̵� �ӵ�")]
     [SerializeField] private float MoveSpeed;
 
     private void Awake()
@@ -20,6 +25,11 @@ public class TraceTarget : MonoBehaviour
         }
     }
 
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+    }
+
     private void FixedUpdate()
     {
         if (target == null)
@@ -28,12 +38,18 @@ public class TraceTarget : MonoBehaviour
         }
 
         Vector3 dir = target.transform.position - transform.position;
-        if (dir.magnitude > distance)
+        float dis = dir.magnitude;
+        if (dis > distance || dis < stopDistance)
         {
+            if(rb.linearVelocityX != 0)
+            {
+                rb.linearVelocityX = 0;
+            }
             return;
         }
 
-        dir.Normalize();
-        rb.linearVelocity = dir * MoveSpeed;
+        float dirX = dir.x > 0 ? 1 : -1;
+
+        rb.linearVelocityX = dirX * MoveSpeed;
     }
 }
