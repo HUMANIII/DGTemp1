8581678f2e15e140e3b87fdae6b32a638606de80
ee <eee@ww.com>
2024-11-30 11:33:31 +0900

    [기능] 게임매니저 생성
    - 싱글톤을 활용한 게임 매니저 생성

---
+++ Assets/2. Script/Manager/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindFirstObjectByType<GameManager>();                
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
